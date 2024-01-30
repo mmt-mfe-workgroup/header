c99cc2ab8d31bbbf8da81ad8026009c3b28646f6
Jean-Christophe Nicolas <mrjcnicolas@gmail.com>
2024-01-30 15:14:52 +0000

    Update RemoteApp.tsx

--- src/RemoteApp.tsx
+++ src/RemoteApp.tsx
@@ -2,5 +2,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 // eslint-disable-next-line react-refresh/only-export-components
-export default (container: HTMLElement, label?: string) =>
-  ReactDOM.render(<App label={label} />, container)
+export default (container: HTMLElement, props?: { label: string }) =>
+  ReactDOM.render(<App {...props} />, container)
