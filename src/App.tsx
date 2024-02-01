7d08e540f0ac7a1d97c74ba3c800329ff88eff76
Elias Carballo <eliascarballo@mmtadmins-MacBook-Pro-2.local>
2024-02-01 13:34:01 +0000

    wip xray event

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,22 @@ function App({ label }: { label?: string }) {
   const [basketTotal, setBasketTotal] = useState(0)
 
   const [clearedBasket, setClearedBasket] = useState(0)
+
   const [voucherCode, setVoucherCode] = useState('')
+
+  const [xrayActive, setXrayActive] = useState(false)
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     window.addEventListener('basketItemCount', (e: any) =>
       setBasketTotal(e?.detail?.basketItemCount)
     )
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window.addEventListener('view:xray', (e: any) => {
+      if (e?.target?.checked) setXrayActive(true)
+      else setXrayActive(false)
+    })
+
     return () => {
       window.removeEventListener('basketItemCount', () => setBasketTotal(0))
     }
@@ -49,55 +58,74 @@ function App({ label }: { label?: string }) {
     <>
       <div className="top-0 left-0">
         <nav className="bg-white w-full z-20 top-0 start-0 border-b border-gray-200">
-          <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
-            <span className="w-1/2 self-center text-4xl font-semibold whitespace-nowrap text-black">
-              {label || 'MFE APP'}
-            </span>
-            <span className="ml-auto flex">
-              <span className="flex mx-2 max-h-13 items-center">
-                <div className="flex">
-                  <input
-                    type="search"
-                    onChange={(e) => setVoucherCode(e?.target?.value)}
-                    id="default-search"
-                    className="mr-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                    placeholder={clickedVocher ? '' : 'Enter code...'}
-                    required
+          {xrayActive && (
+            <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4 bg-green-400">
+              <span className="w-1/2 self-center text-base font-semibold whitespace-nowrap text-black">
+                <p>
+                  CDN_LOCATION =
+                  https://mmt-mfe-header.netlify.app/assets/remoteEntry.js
+                </p>
+                <p>FRAMEWORK = REACT@18.2</p>
+                <p>APP NAME = HEADER</p>
+                <p>
+                  EVENTS - DISPATCHED "addVoucher", "clearBasket" EVENTS -
+                  SUBSCRIBED "view:xray"
+                </p>
+              </span>
+            </div>
+          )}
+
+          {!xrayActive && (
+            <div className="max-w-screen-xl flex flex-wrap items-center mx-auto p-4">
+              <span className="w-1/2 self-center text-4xl font-semibold whitespace-nowrap text-black">
+                {label || 'MFE APP'}
+              </span>
+              <span className="ml-auto flex">
+                <span className="flex mx-2 max-h-13 items-center">
+                  <div className="flex">
+                    <input
+                      type="search"
+                      onChange={(e) => setVoucherCode(e?.target?.value)}
+                      id="default-search"
+                      className="mr-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                      placeholder={clickedVocher ? '' : 'Enter code...'}
+                      required
+                    />
+                    <ErrorBoundary>
+                      <Suspense fallback={<Loader />}>
+                        <RemoteButton
+                          label={
+                            clickedVocher ? 'Voucher applied' : 'Apply voucher'
+                          }
+                          onClick={() => handleVoucherClick(voucherCode)}
+                        />
+                      </Suspense>
+                    </ErrorBoundary>
+                  </div>
+                </span>
+                <span className="flex mx-2 max-h-13 items-center ml-2">
+                  <span className="w-32 mr-9">
+                    <ErrorBoundary>
+                      <Suspense fallback={<Loader />}>
+                        <RemoteButton
+                          label="Clear basket"
+                          onClick={() => handleClearBasketClick()}
+                        />
+                      </Suspense>
+                    </ErrorBoundary>
+                  </span>
+                  <img
+                    className="max-h-12 max-w-8"
+                    src={basketTotal ? basketSelected : basket}
+                    alt="basket"
                   />
-                  <ErrorBoundary>
-                    <Suspense fallback={<Loader />}>
-                      <RemoteButton
-                        label={
-                          clickedVocher ? 'Voucher applied' : 'Apply voucher'
-                        }
-                        onClick={() => handleVoucherClick(voucherCode)}
-                      />
-                    </Suspense>
-                  </ErrorBoundary>
-                </div>
-              </span>
-              <span className="flex mx-2 max-h-13 items-center ml-2">
-                <span className="w-32 mr-9">
-                  <ErrorBoundary>
-                    <Suspense fallback={<Loader />}>
-                      <RemoteButton
-                        label="Clear basket"
-                        onClick={() => handleClearBasketClick()}
-                      />
-                    </Suspense>
-                  </ErrorBoundary>
-                </span>
-                <img
-                  className="max-h-12 max-w-8"
-                  src={basketTotal ? basketSelected : basket}
-                  alt="basket"
-                />
-                <span className="mb-6 text-black">
-                  {basketTotal || clearedBasket}
+                  <span className="mb-6 text-black">
+                    {basketTotal || clearedBasket}
+                  </span>
                 </span>
               </span>
-            </span>
-          </div>
+            </div>
+          )}
         </nav>
       </div>
     </>
