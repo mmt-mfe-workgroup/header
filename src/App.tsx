d05bdbbc4ce03cac645e33e027808cb3d8b79604
Elias Carballo <eliascarballo@mmtadmins-MacBook-Pro-2.local>
2024-02-01 12:33:56 +0000

    wip stlying

--- src/App.tsx
+++ src/App.tsx
@@ -58,13 +58,10 @@ function App({ label }: { label?: string }) {
                 <div className="flex">
                   <input
                     type="search"
-                    onChange={(e) => {
-                      console.log(e?.target?.value)
-                      setVoucherCode(e?.target?.value)
-                    }}
+                    onChange={(e) => setVoucherCode(e?.target?.value)}
                     id="default-search"
-                    className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Enter code"
+                    className="ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder={clickedVocher ? '' : 'Enter code...'}
                     required
                   />
                   <ErrorBoundary>
